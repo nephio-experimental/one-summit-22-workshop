f5432be2cbfb087d1e89d1f0febdc11fda25692e
sandeep <ssharma@aarnanetworks.com>
2022-11-08 14:30:21 +0000

    Fixing shellcheck source file error

--- ansible_kind/scripts/multiclusterkind/kind_create_clusters.sh
+++ ansible_kind/scripts/multiclusterkind/kind_create_clusters.sh
@@ -4,12 +4,11 @@ set -o nounset
 set -o pipefail
 
 # Parameters: [HOST_IPADDRESS](optional) if you want to export clusters' API server port to specific IP address
-# This script depends on utils in: ${REPO_ROOT}/hack/util.sh
 # 1. used by developer to setup develop environment quickly.
 # 2. used by e2e testing to setup test environment automatically.
 
-REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")
-source "${REPO_ROOT}"/hack/util.sh
+# shellcheck source=util.sh
+source util.sh
 
 # variable define
 KUBECONFIG_PATH=${KUBECONFIG_PATH:-"${HOME}/.kube"}
