c9a966f3f742ddb055f7930911b7b506536da28b
shailendrarjpt <r.shailendra@samsung.com>
2023-04-20 14:13:06 +0530

    Removed previous changes

--- nephio-ansible-install-offline/installer_lib/blueprint_push.sh
+++
@@ -1,8 +0,0 @@
-cd $1
-git init
-git remote add origin http://localhost:3000/nephio/$2.git
-git pull origin main
-git checkout -b main
-git add *
-git commit -m "first commit"
-git push -f http://nephio:$3@localhost:3000/nephio/$2.git
